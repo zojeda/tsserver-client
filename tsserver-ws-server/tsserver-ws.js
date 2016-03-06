f2a49a6991501ca0ee06dc1764f361743d8f83d7
Zacarías F. Ojeda <zojeda@gmail.com>
2016-03-06 02:14:00 -0300

    adding a typescript dependency

--- tsserver-ws-server/tsserver-ws.js
+++ tsserver-ws-server/tsserver-ws.js
@@ -1,26 +1,27 @@
 var ws = require("nodejs-websocket")
 
+var path = require("path");
 var proc = require("child_process");
-var tsserver = proc.spawn("tsserver");
 
+var tsserver = proc.spawn(path.join(__dirname, "..", "node_modules", "typescript", "bin", "tsserver"));
 
 var server = ws.createServer(function (conn) {
-    console.log("New connection")
-    tsserver.stdout.on('data', function(data) {
-      console.log("Sending "+data)
-      try {
-        conn.sendText(data);
-      } catch(e) {
-        console.error("connection closed");
-      }
-    });
-    conn.on("text", function (str) {
-        console.log("Received "+str)
-        tsserver.stdin.write(str);
-    })
-    conn.on("close", function (code, reason) {
-        console.log("Connection closed")
-    })
+  console.log("New connection")
+  tsserver.stdout.on('data', function (data) {
+    console.log("Sending " + data)
+    try {
+      conn.sendText(data);
+    } catch (e) {
+      console.error("connection closed");
+    }
+  });
+  conn.on("text", function (str) {
+    console.log("Received " + str)
+    tsserver.stdin.write(str);
+  })
+  conn.on("close", function (code, reason) {
+    console.log("Connection closed")
+  })
 }).listen(8001)
 
 console.log("listenting for ws connetings on port 8001");
